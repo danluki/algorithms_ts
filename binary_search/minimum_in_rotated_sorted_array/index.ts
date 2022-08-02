a4bd9acfac3e49fd5c9b950fbeda1e7954229947
danluki02 <danluki02@yandex.ru>
2022-08-02 09:05:38 +0500

    minimum in rotated sorted array

--- binary_search/minimum_in_rotated_sorted_array/index.ts
+++ binary_search/minimum_in_rotated_sorted_array/index.ts
@@ -1,9 +1,28 @@
 function findMin(nums: number[]): number {
+
+  if(nums.length === 1) return nums[0];
+
   let left = 0,
     right = nums.length - 1,
-    min = Number.MAX_VALUE;
+    middle = 0;
 
-  while (left <= right) {}
+  while (left <= right) {
+    middle = (left + right) >> 1;
+    
+    if(nums[left] < nums[right]){
+      return nums[left];
+    }
+
+    if(nums[left] <= nums[middle]){
+      left = middle + 1;
+    }
+
+    if(nums[middle] < nums[left]){
+      right = middle;
+    }
+  }
+
+  return nums[left];
 }
 
 //1
